c33d415ad3f253a679218c8d7f9a10402d91da91
Vika <krivosheevaviktoria98@gmail.com>
2019-07-25 18:03:58 +0300

    Create data model

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -8,5 +8,20 @@
 
 import UIKit
 
-
+class Meal {
+    // MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        if(name.isEmpty || rating < 0) {
+            return nil
+        }
+        
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
 
